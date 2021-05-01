3d50dec45fe06d5e7fbc8a3e48ca1d80fcb45b48
Claudio Jara Yañez <cjarayan@everis.com>
2021-04-30 20:35:34 -0400

    Cambio en app js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save testing if refresh automatic shiteeesssssss plzzz, testin again
+          Edit <code>src/App.js</code> esto es la feature branch
         </p>
         <a
           className="App-link"
